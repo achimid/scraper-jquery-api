7a6c26b3e70a98122ce96a42d6e13c139852dbb4
Lourran Zorzi <lourranzorzi@softbox.com.br>
2019-12-24 12:14:57 -0300

    cache

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const puppeteer = require('puppeteer');
 const compression = require('compression');
 
-const urlJquery = {url: 'https://code.jquery.com/jquery-3.2.1.min.js'}
+const urlJquery = { url: 'https://code.jquery.com/jquery-3.2.1.min.js' }
 const defaults = {
-    script: '$("html").html();',
-    importJquery: true
+    script: '$("html").html()',
+    importJquery: true,
+    waitTime: false
 }
+
+const repository = {}
 
 const app = express()
 let browser = null
@@ -14,41 +17,59 @@ let browser = null
 app.use(compression());
 app.use(express.json())
 
-function validateNotNull(value, message)  {
+function validateNotNull(value, message) {
     if (!value) {
-        throw { erro: message}
+        throw { erro: message }
     }
 }
 
-const preparePage = ({ importJquery = defaults.importJquery }) => async (page) => {
-    if (importJquery) 
-        await page.addScriptTag(urlJquery)
-    
-    return page    
-}
+const scraperUrl = ({ url, options }) => {
+    validateNotNull(url, 'Url não pode ser null')
+    const opts = Object.assign(defaults, options)
 
-const scraperUrl = ({url, options}) => {    
-    validateNotNull(url)    
+    const jsonHash = JSON.stringify({ url, options})
+    const contentCache = repository[jsonHash]
+    const hasCache = !!contentCache
+
+    if (hasCache)
+        return Promise.resolve({retorno: contentCache })
 
     return browser.newPage()
-        .then(preparePage(options))
-        .then(async (page) => {
-            await page.goto(url)
-            const retorno = await page.evaluate(options.script || defaults.script)
-            await page.close()
-            return retorno
+        .then(async (page) => {            
+            try {    
+                const response = {}            
+
+                await page.goto(url)                                 
+
+                if (opts.importJquery) await page.addScriptTag(urlJquery)
+                if (opts.waitTime) await page.waitFor(opts.waitTime)
+
+                response.retorno = await page.evaluate(opts.script)
+                await page.close()
+                
+                repository[jsonHash] = response.retorno
+
+                return response
+            } catch (error) {
+                Promise.reject(error)
+          }            
         })
 }
 
 app.post('/evaluate', (req, res) => {
     return scraperUrl(req.body)
-        .then(response => res.send(response))
-        .catch(erro => res.send(erro))
+        .then(response => res.send({response: response, sucess:'true' }))
+        .catch(erro => res.send({erro}))
 })
 
 const initBrowser = async () => {
     browser = await puppeteer.launch();
 }
 
-init()
+app.use(function(err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+initBrowser()
 app.listen(process.env.PORT || 9000)
