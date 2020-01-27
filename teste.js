4ebd5ffd35e11a21aa02c6614ff7d9f1ec7ddef1
Lourran Zorzi <lourranzorzi@softbox.com.br>
2020-01-27 11:30:06 -0300

    all

---
+++ teste.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+(async () => {
+     const browser = await puppeteer.launch({
+        ignoreHTTPSErrors: true,
+        args: [
+            '--proxy-server=http://173.249.35.163:10010'
+        ]
+    });
+    const page = await browser.newPage();
+
+    // await page.setExtraHTTPHeaders({
+    //     'Proxy-Authorization': 'Basic ' + Buffer.from(':').toString('base64'),
+    // });
+    
+    console.log('Opening page ...');
+    try {
+        await page.goto('https://httpbin.scrapinghub.com/redirect/6', {timeout: 15000})
+    } catch(err) {
+        console.log(err);
+    }
+  
+    console.log('Taking a screenshot ...');
+    await page.screenshot({path: 'screenshot.png', fullPage: true});
+    await browser.close();
+})();
